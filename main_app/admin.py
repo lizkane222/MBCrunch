995de5e6842d1deaf97093d33df70db544401c17
Liz Kane <lizkane222@gmail.com>
2020-10-06 18:01:55 -0700

    reorganizing models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Bin, Investor
+from .models import Bin, Investor, Collection_Market
 
 # Register your models here.
 
 admin.site.register(Bin)
 admin.site.register(Investor)
+admin.site.register(Collection_Market)
--- main_app/models.py
+++ main_app/models.py
@@ -31,37 +31,45 @@ class Bin(models.Model):
         default=BINTYPES[0][0]
     )
     location = models.CharField(max_length = 200)
-    collection_market = models.TextField(max_length=550)
-    delivery_date = models.IntegerField()
+    delivery_date = models.DateField()
     delivery_location = models.CharField(max_length=200)
     verified = models.BooleanField(True == 'accurate bin type', False =='inaccurate bin type')
-    sponsor = models.CharField(max_length=100)
     gives_rewards = models.IntegerField()
+    collection_market = models.TextField(max_length=550)
+    sponsor = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.get_type_display} is at {self.location}."
+        return f"{self.get_type_display()} is at {self.location}."
 
 
 class Investor(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=500)
     deposits_made = models.IntegerField()
-    community_contributions = models.IntegerField()
     verifications = models.CharField(
         max_length = 2,
         choices=VERIFYBIN,
         default=VERIFYBIN[0][0]
     )
     current_location = models.CharField(max_length=200)
-    sponsor = models.CharField(max_length=200)
     identify_new_bin = models.CharField(max_length=200)
     gives_rewards = models.IntegerField()
+    community_contributions = models.IntegerField()
+    sponsor = models.CharField(max_length=200)
 
     bin = models.ForeignKey(Bin, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}: Total Deposits: {self.deposits_made} Community Contribution:{self.community_contribution_points}!  Verified: {self.get_verifications_display} bins!{self.current_location} Sponsoring {self.sponsor} bins. Rewards:{self.gives_rewards}"
 
+
+class Collection_Market(models.Model):
+    name = models.CharField(max_length=200)
+    dates = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    assoc_bins = models.Bin
+    assoc_investors = models.Investor
 
 
 
